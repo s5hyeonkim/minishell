dee2985a15619837495f1724d04c45745614664b
sn25yb <sn25yb_code@gmail.com>
2024-06-05 16:18:34 +0900

    feat: minishell.h: 허용함수 헤더파일 추가

--- minishell.h
+++ minishell.h
@@ -1,13 +1,33 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-//minishell 허용함수
+//허용함수 헤더
+# include <readline/readline.h> 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <string.h>
+# include <stdio.h>
+# include <errno.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
 
-#include "libft/libft.h"
+//
+# include "libft/libft.h"
+# include "ft_err.h"
 
 //기타함수
-#include <stdio.h> //printf
-#include "ft_err.h"
+
 
 typedef enum e_boolean
 {
