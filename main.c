63accf083d1eff09f185606a490ed23af7d4c783
sn25yb <sn25yb_code@gmail.com>
2024-07-11 18:39:52 +0900

    refactor: setting.c : 함수 이동

--- main.c
+++ main.c
@@ -32,24 +32,6 @@ char	**get_env_paths(char *envp[])
 	return (ret);
 }
 
-void	set_data(t_shell *shell, char *envp[])
-{
-	shell->data.envps = strstodeq(envp);
-	shell->data.paths = get_env_paths(envp);
-	if (shell->data.envps)
-	{
-		shell->data.home = ft_strdup(read_val_deq(shell->data.envps, "HOME"));
-		shell->data.user = ft_strdup(read_val_deq(shell->data.envps, "USER"));
-	}
-	if (!shell->data.paths || !shell->data.envps || !shell->data.home || !shell->data.user)
-		exit_process(shell, NULL, EXTRA_ERROR);
-}
-
-void	set_shell(t_shell *shell, char *envp[])
-{
-	ft_memset(shell, 0, sizeof(t_shell));
-	set_data(shell, envp);
-}
 
 void	check_valid(t_shell *shell, int argc)
 {
